9c689e5998884a62ce4ba7723a43d0d28f7c2762
sarmi-23 <sharmilaaa23@gmail.com>
2024-10-26 12:14:12 +0530

    Add files via upload

---
+++ papp/dist/js/config.js
@@ -0,0 +1,8 @@
+window.config = {
+    name: '#{GITHUB_NAME}#',
+    title: '#{GITHUB_TITLE}#',
+    bio: '#{GITHUB_BIO}#',
+    location: '#{GITHUB_LOCATION}#',
+    email: '#{GITHUB_EMAIL}#',
+    image: '#{GITHUB_IMAGE}#'
+};
